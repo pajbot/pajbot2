82cfbc9ad34c9d696eab4b8a00bec7f80de5b538
Rasmus Karlsson <pajlada@bithack.se>
2016-05-27 11:15:02 +0000

    Config system moved to a json file.

--- main.go
+++ main.go
@@ -1,22 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"time"
 
-	"github.com/nuuls/pajbot2/config"
 	"github.com/nuuls/pajbot2/irc"
 )
 
+/*
+The Config contains all the data required to connect
+to the twitch IRC servers
+*/
+type Config struct {
+	Pass       string `json:"pass"`
+	Nick       string `json:"nick"`
+	BrokerPort string `json:"broker_port"`
+
+	RedisHost     string `json:"redis_host"`
+	RedisPassword string `json:"redis_password"`
+
+	TLSKey  string `json:"tls_key"`
+	TLSCert string `json:"tls_cert"`
+
+	Channels []string `json:"channels"`
+
+	ToWeb   chan map[string]interface{}
+	FromWeb chan map[string]interface{}
+}
+
+func loadConfig(path string) (*Config, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	config := &Config{}
+	err = json.Unmarshal(file, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
 func main() {
-	config := config.GetConfig()
+	// TODO: Use config path from system arguments
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatal("An error occured while loading the config file:", err)
+	}
+
 	irc := irc.Init(config.Pass, config.Nick)
-	irc.JoinChannel("nuuls")
-	//irc.JoinChannel("forsenlol")
-	irc.JoinChannel("pajlada")
-	// irc.JoinChannel("lirik")
-	// irc.JoinChannel("timthetatman")
-	// irc.JoinChannel("witwix")
-	// irc.JoinChannel("watchmeblink1")
+
+	for _, channel := range config.Channels {
+		irc.JoinChannel(channel)
+	}
+
 	for {
 		time.Sleep(5 * time.Second)
 	}
