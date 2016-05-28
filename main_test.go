b35a37d4d5b7259eebe3078569100d1222cc80ce
Rasmus Karlsson <pajlada@bithack.se>
2016-05-28 23:55:56 +0000

    Added tests for the main package

---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/pajlada/pajbot2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	var configTests = []struct {
+		inputPath string
+		expectedC *main.Config
+		expectedE bool
+	}{
+		{
+			inputPath: "resources/testfiles/config1.json",
+			expectedC: &main.Config{Pass: "oauth:xD", Nick: "twitch_username", BrokerPort: "", RedisHost: "", RedisPassword: "", TLSKey: "", TLSCert: "", Channels: []string{"pajlada", "nuuls", "forsenlol"}, ToWeb: (chan map[string]interface{})(nil), FromWeb: (chan map[string]interface{})(nil)},
+			expectedE: false,
+		},
+		{
+			inputPath: "resources/testfiles/nonexistingconfigfile.json",
+			expectedC: nil,
+			expectedE: true,
+		},
+		{
+			inputPath: "resources/testfiles/config2_invalidjson.json",
+			expectedC: nil,
+			expectedE: true,
+		},
+	}
+
+	for _, tt := range configTests {
+		res, err := main.LoadConfig(tt.inputPath)
+
+		if tt.expectedE {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+
+		assert.Equal(t, tt.expectedC, res)
+	}
+}
