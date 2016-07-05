f4a694009840dbed311d980403d9117ccfab8f37
nuuls <nilsv21@gmail.com>
2016-07-06 01:25:57 +0200

    dont stream retweets and quoted tweets

--- pbtwitter/streaming.go
+++ pbtwitter/streaming.go
@@ -8,6 +8,10 @@ import (
 
 // Follow follows given users timeline stream
 func (bot *Bot) Follow(user string) {
+	if user == "ALL" {
+		bot.Following = bot.Client.followedUsers
+		return
+	}
 	u := strings.ToLower(user)
 	for _, usr := range bot.Following {
 		if usr == u {
@@ -20,6 +24,12 @@ func (bot *Bot) Follow(user string) {
 
 func (c *Client) streamToBots(tweet *twitter.Tweet) {
 	log.Debug(tweet.Text)
+	log.Debug(tweet.Retweeted)
+	log.Debugf("%+v", tweet)
+	if tweet.RetweetedStatus != nil || tweet.QuotedStatus != nil {
+		log.Debug("RETWEETED OR QUOTED TWEET, NOT STREAMING")
+		return
+	}
 	for _, bot := range c.Bots {
 		for _, followedUser := range bot.Following {
 			if strings.ToLower(tweet.User.ScreenName) == followedUser {
