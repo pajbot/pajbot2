045b4d4b087cf36dc8713ac62ba2eda00ed189cd
Rasmus Karlsson <pajlada@bithack.se>
2016-09-14 19:22:29 +0000

    we now use gobttv for bttv api requests

--- bot/emotes.go
+++ bot/emotes.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/pajlada/gobttv"
 	"github.com/pajlada/goffz"
 	"github.com/pajlada/pajbot2/apirequest"
 	"github.com/pajlada/pajbot2/common"
@@ -15,16 +16,23 @@ import (
 var GlobalEmotes common.ExtensionEmotes
 
 func loadGlobalBttvEmotes() {
-	globalEmotes, err := apirequest.BTTVAPI.LoadEmotes("global")
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	GlobalEmotes.Bttv = make(map[string]common.Emote)
-	for _, emote := range globalEmotes {
-		GlobalEmotes.Bttv[emote.Name] = emote
-	}
-	GlobalEmotes.BttvLastUpdate = time.Now()
+	apirequest.BTTV.GetEmotes(
+		func(emotesResponse gobttv.EmotesResponse) {
+			GlobalEmotes.Bttv = make(map[string]common.Emote)
+			GlobalEmotes.BttvLastUpdate = time.Now()
+
+			for _, emote := range emotesResponse.Emotes {
+				GlobalEmotes.Bttv[emote.Regex] = ParseBTTVGlobalEmote(emote)
+			}
+		},
+		func(statusCode int, statusMessage, errorMessage string) {
+			log.Errorf("Error fetching Global BTTV Emotes")
+			log.Errorf("Status code: %d", statusCode)
+			log.Errorf("Status message: %s", statusMessage)
+			log.Errorf("Error message: %s", errorMessage)
+		}, func(err error) {
+			log.Errorf("Internal error: %s", err)
+		})
 }
 
 func loadGlobalFrankerFaceZEmotes() {
@@ -57,17 +65,26 @@ func LoadGlobalEmotes() {
 
 // LoadBttvEmotes should load emotes from redis, but this should do for now
 func (bot *Bot) LoadBttvEmotes() {
-	channelEmotes, err := apirequest.BTTVAPI.LoadEmotes(bot.Channel.Name)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	bot.Channel.Emotes.BttvLastUpdate = time.Now()
+	apirequest.BTTV.GetChannel(bot.Channel.Name,
+		func(channel gobttv.ChannelResponse) {
+			bot.Channel.Emotes.Bttv = make(map[string]common.Emote)
+			bot.Channel.Emotes.BttvLastUpdate = time.Now()
 
-	bot.Channel.Emotes.Bttv = make(map[string]common.Emote)
-	for _, emote := range channelEmotes {
-		bot.Channel.Emotes.Bttv[emote.Name] = emote
-	}
+			for _, emote := range channel.Emotes {
+				bot.Channel.Emotes.Bttv[emote.Code] = ParseBTTVChannelEmote(emote)
+			}
+		},
+		func(statusCode int, statusMessage, errorMessage string) {
+			// We ignore 404 errors, it just means he doesn't have a BTTV account
+			if statusCode != 404 {
+				log.Errorf("Error fetching Channel BTTV Emotes (%s)", bot.Channel.Name)
+				log.Errorf("Status code: %d", statusCode)
+				log.Errorf("Status message: %s", statusMessage)
+				log.Errorf("Error message: %s", errorMessage)
+			}
+		}, func(err error) {
+			log.Errorf("Internal error: %s", err)
+		})
 }
 
 // regex would probably be better but im a regex noob ¯\_(ツ)_/¯
@@ -102,6 +119,36 @@ func ParseFrankerFaceZEmote(emote goffz.EmoteData) common.Emote {
 		SizeX: emote.Width,
 		SizeY: emote.Height,
 		Count: 1,
+	}
+}
+
+// ParseBTTVGlobalEmote parses a BTTV emote into a common.Emote
+func ParseBTTVGlobalEmote(emote gobttv.GlobalEmoteData) common.Emote {
+	spl := strings.Split(emote.URL, "/emote/")[1]
+	id := spl[:len(spl)-3] // remove /1x
+	isGif := emote.ImageType == "gif"
+	return common.Emote{
+		Name:  emote.Regex,
+		ID:    id,
+		Type:  "bttv",
+		SizeX: emote.Width,
+		SizeY: emote.Height,
+		Count: 1,
+		IsGif: isGif,
+	}
+}
+
+// ParseBTTVChannelEmote parses a BTTV emote into a common.Emote
+func ParseBTTVChannelEmote(emote gobttv.ChannelEmoteData) common.Emote {
+	isGif := emote.ImageType == "gif"
+	return common.Emote{
+		Name:  emote.Code,
+		ID:    emote.ID,
+		Type:  "bttv",
+		SizeX: 28,
+		SizeY: 28,
+		Count: 1,
+		IsGif: isGif,
 	}
 }
 
