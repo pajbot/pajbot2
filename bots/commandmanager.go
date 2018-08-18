db8a2d95f4957f0401dc18803b43fa9580872ec8
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-08-18 15:48:22 +0200

    Remove more unused code

--- bots/commandmanager.go
+++
@@ -1,8 +0,0 @@
-package bots
-
-// CommandManager xD
-type CommandManager struct {
-	// Character required before a command
-	// Usually 1 character, can be more though
-	Prefix string
-}
--- filter/link.go
+++
@@ -1,7 +0,0 @@
-package filter
-
-// LinkFilter xD
-func LinkFilter(m string) []string {
-	// return xurls.Relaxed.FindAllString(m, -1)
-	return []string{}
-}
