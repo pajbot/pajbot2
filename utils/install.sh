69a7db933e7bc4bbfef1aa349365b8837c5d1f05
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-10-14 13:15:53 +0200

    xd

---
+++ utils/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# Compile C++ "coreruncommon" lib
+cd 3rdParty/MessageHeightTwitch/c-interop
+g++ -c coreruncommon.cpp
+ar rvs libcoreruncommon.a coreruncommon.o
+
+# Compile C# library
+cd ../
+dotnet build --configuration Release -o build/
+
+cp charmap.bin.gz build/MessageHeightTwitch.dll ../../cmd/bot/
