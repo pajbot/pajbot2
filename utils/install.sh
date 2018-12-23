a0503844dd6e64c4a27593793f8a152c08fdf6b7
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-12-23 19:17:40 +0100

    update install folder to simplify dll-importing
    thanks Randers00!

--- utils/install.sh
+++ utils/install.sh
@@ -9,6 +9,6 @@ ar rvs libcoreruncommon.a coreruncommon.o
 
 # Compile C# library
 cd ../
-dotnet build --configuration Release -o build/
+dotnet publish --configuration Release -o build/
 
-cp charmap.bin.gz build/MessageHeightTwitch.dll ../../cmd/bot/
+cp charmap.bin.gz build/*.dll ../../cmd/bot/
