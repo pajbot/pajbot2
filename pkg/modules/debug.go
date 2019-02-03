e73d41a4fa57ff4f2d8afccbfb41a7f8e592d388
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-02-03 08:41:03 +0100

    add a debug module which allows admins to use !pb2say

---
+++ pkg/modules/debug.go
@@ -0,0 +1,88 @@
+package modules
+
+import (
+	"strings"
+
+	"github.com/pajlada/pajbot2/pkg"
+)
+
+type pb2Say struct {
+}
+
+func (c pb2Say) Trigger(bot pkg.Sender, botChannel pkg.BotChannel, parts []string, channel pkg.Channel, user pkg.User, message pkg.Message, action pkg.Action) {
+	if !user.HasPermission(botChannel.Channel(), pkg.PermissionAdmin) {
+		return
+	}
+
+	bot.Say(botChannel.Channel(), strings.Join(parts[1:], " "))
+}
+
+func init() {
+	Register(debugModuleSpec)
+}
+
+type debugModule struct {
+	botChannel pkg.BotChannel
+
+	server *server
+
+	commands map[string]pkg.CustomCommand
+}
+
+var debugModuleSpec = &moduleSpec{
+	id:               "debug",
+	name:             "Debug",
+	maker:            newDebugModule,
+	enabledByDefault: true,
+}
+
+func newDebugModule() pkg.Module {
+	return &debugModule{
+		server: &_server,
+
+		commands: make(map[string]pkg.CustomCommand),
+	}
+}
+
+func (m *debugModule) registerCommand(aliases []string, command pkg.CustomCommand) {
+	for _, alias := range aliases {
+		m.commands[alias] = command
+	}
+}
+
+func (m *debugModule) Initialize(botChannel pkg.BotChannel, settings []byte) error {
+	m.botChannel = botChannel
+
+	m.registerCommand([]string{"!pb2say"}, &pb2Say{})
+
+	return nil
+}
+
+func (m *debugModule) Disable() error {
+	return nil
+}
+
+func (m *debugModule) Spec() pkg.ModuleSpec {
+	return debugModuleSpec
+}
+
+func (m *debugModule) BotChannel() pkg.BotChannel {
+	return m.botChannel
+}
+
+func (m *debugModule) OnWhisper(bot pkg.Sender, source pkg.User, message pkg.Message) error {
+	return nil
+}
+
+func (m *debugModule) OnMessage(bot pkg.Sender, channel pkg.Channel, user pkg.User, message pkg.Message, action pkg.Action) error {
+	parts := strings.Split(message.GetText(), " ")
+	if len(parts) == 0 {
+		return nil
+	}
+
+	if command, ok := m.commands[strings.ToLower(parts[0])]; ok {
+		command.Trigger(bot, m.botChannel, parts, channel, user, message, action)
+	}
+
+	return nil
+}
