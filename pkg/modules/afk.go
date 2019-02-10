8928bd35452a92eb2b5be8c18b95ff2a1c50ce02
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-02-10 14:28:24 +0100

    add test afk module that uses botsync

---
+++ pkg/modules/afk.go
@@ -0,0 +1,173 @@
+package modules
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/pajlada/botsync/pkg/client"
+	"github.com/pajlada/botsync/pkg/protocol"
+	"github.com/pajlada/pajbot2/pkg"
+)
+
+func init() {
+	Register(afkSpec)
+}
+
+var (
+	afkDatabase = map[string]bool{}
+)
+
+type afk struct {
+	botChannel pkg.BotChannel
+
+	server *server
+
+	commands map[string]pkg.CustomCommand
+
+	botsync *client.Client
+}
+
+var afkSpec = &moduleSpec{
+	id:               "afk",
+	name:             "AFK",
+	maker:            newAFK,
+	enabledByDefault: false,
+}
+
+func newAFK() pkg.Module {
+	m := &afk{
+		server: &_server,
+
+		commands: make(map[string]pkg.CustomCommand),
+
+		botsync: client.NewClient("ws://localhost:8080/ws/pubsub"),
+	}
+
+	m.botsync.OnMessage(m.onMessage)
+
+	m.botsync.OnConnect(func() {
+		fmt.Println("Connected to botsync")
+		m.botsync.Send(protocol.NewAFKSubscribeMessage(m.botChannel.ChannelID()))
+		m.botsync.Send(protocol.NewBackSubscribeMessage(m.botChannel.ChannelID()))
+	})
+
+	return m
+}
+
+type afkCmd struct {
+	m *afk
+}
+
+func (c *afkCmd) Trigger(botChannel pkg.BotChannel, parts []string, channel pkg.Channel, user pkg.User, message pkg.Message, action pkg.Action) {
+	if user.IsModerator() || user.IsBroadcaster(channel) {
+		c.m.botsync.Send(protocol.NewAFKMessage(&protocol.AFKParameters{
+			UserID:   user.GetID(),
+			UserName: user.GetName(),
+
+			Reason: strings.Join(parts[1:], " "),
+
+			ChannelID:   botChannel.ChannelID(),
+			ChannelName: botChannel.ChannelName(),
+		}))
+	}
+}
+
+func (m *afk) registerCommand(aliases []string, command pkg.CustomCommand) {
+	for _, alias := range aliases {
+		m.commands[alias] = command
+	}
+}
+
+func (m *afk) Initialize(botChannel pkg.BotChannel, settings []byte) error {
+	m.botChannel = botChannel
+
+	m.registerCommand([]string{"!afk", "!gn"}, &afkCmd{m})
+
+	m.botsync.SetAuthentication(protocol.Authentication{
+		TwitchUserID:        botChannel.Bot().TwitchAccount().ID(),
+		AuthenticationToken: "penis",
+	})
+
+	go func() {
+		for {
+			err := m.botsync.Connect()
+			if err != nil {
+				if err == client.ErrDisconnected {
+					return
+				}
+			}
+
+			// wait a second before attempting to reconnect
+			<-time.After(time.Second)
+		}
+	}()
+
+	return nil
+}
+
+func (m *afk) onMessage(message *protocol.Message) {
+	switch message.Topic {
+	case "afk":
+		parameters := protocol.AFKParameters{}
+		err := json.Unmarshal(message.Data, &parameters)
+		if err != nil {
+			fmt.Println("Error parsing afk message:", err)
+			return
+		}
+		m.botChannel.Say(parameters.UserName + " just went afk: " + parameters.Reason)
+		afkDatabase[parameters.UserID] = true
+
+	case "back":
+		parameters := protocol.BackParameters{}
+		err := json.Unmarshal(message.Data, &parameters)
+		if err != nil {
+			fmt.Println("Error parsing afk message:", err)
+			return
+		}
+		m.botChannel.Say(parameters.UserName + " just went came back from being afk (" + parameters.AFKChannelName + "): " + parameters.Reason)
+		delete(afkDatabase, parameters.UserID)
+	}
+}
+
+func (m *afk) Disable() error {
+	m.botsync.Disconnect()
+	return nil
+}
+
+func (m *afk) Spec() pkg.ModuleSpec {
+	return afkSpec
+}
+
+func (m *afk) BotChannel() pkg.BotChannel {
+	return m.botChannel
+}
+
+func (m *afk) OnWhisper(bot pkg.BotChannel, source pkg.User, message pkg.Message) error {
+	return nil
+}
+
+func (m *afk) OnMessage(bot pkg.BotChannel, user pkg.User, message pkg.Message, action pkg.Action) error {
+	if _, ok := afkDatabase[user.GetID()]; ok {
+		m.botsync.Send(protocol.NewBackMessage(&protocol.BackParameters{
+			UserID:   user.GetID(),
+			UserName: user.GetName(),
+
+			ChannelID:   bot.ChannelID(),
+			ChannelName: bot.ChannelName(),
+		}))
+		return nil
+	}
+
+	parts := strings.Split(message.GetText(), " ")
+	if len(parts) == 0 {
+		return nil
+	}
+
+	if command, ok := m.commands[strings.ToLower(parts[0])]; ok {
+		command.Trigger(m.botChannel, parts, bot.Channel(), user, message, action)
+	}
+
+	return nil
+}
