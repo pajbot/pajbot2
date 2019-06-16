8549f6009970d2e2621bb8336142d779228792df
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-06-16 10:47:14 +0200

    use command manager for the command matching in the Value module

--- pkg/modules/value.go
+++ pkg/modules/value.go
@@ -1,9 +1,8 @@
 package modules
 
 import (
-	"strings"
-
 	"github.com/pajbot/pajbot2/pkg"
+	"github.com/pajbot/pajbot2/pkg/commands"
 	"github.com/pajbot/pajbot2/pkg/twitchactions"
 	"github.com/pajbot/utils"
 )
@@ -38,60 +37,58 @@ func init() {
 type value struct {
 	base
 
+	commands pkg.CommandsManager
+
 	A float32
 	B float32
+}
+
+type valueCmd struct {
+	m     *value
+	key   string
+	value *float32
+}
+
+func (c *valueCmd) set(parts []string, event pkg.MessageEvent) pkg.Actions {
+	if err := c.m.setParameter(c.key, parts[1]); err != nil {
+		return twitchactions.Mention(event.User, err.Error())
+	}
+
+	err := c.m.Save()
+	if err != nil {
+		return nil
+	}
+	return twitchactions.Mention(event.User, c.key+" set to "+utils.Float32ToString(*c.value))
+}
+
+func (c *valueCmd) get(parts []string, event pkg.MessageEvent) pkg.Actions {
+	return twitchactions.Mention(event.User, c.key+" is "+utils.Float32ToString(*c.value))
+}
+
+func (c *valueCmd) Trigger(parts []string, event pkg.MessageEvent) pkg.Actions {
+	if len(parts) >= 2 {
+		return c.set(parts, event)
+	}
+
+	return c.get(parts, event)
 }
 
 func newValue(b base) pkg.Module {
 	m := &value{
 		base: b,
+
+		commands: commands.NewCommands(),
 	}
 
 	m.parameters["A"].Link(&m.A)
 	m.parameters["B"].Link(&m.B)
 
+	m.commands.Register([]string{"!value-a"}, &valueCmd{m, "A", &m.A})
+	m.commands.Register([]string{"!value-b"}, &valueCmd{m, "B", &m.B})
+
 	return m
 }
 
 func (m *value) OnMessage(event pkg.MessageEvent) pkg.Actions {
-	user := event.User
-	message := event.Message
-
-	if !strings.HasPrefix(message.GetText(), "!") {
-		return nil
-	}
-	parts := strings.Split(message.GetText(), " ")
-	if parts[0] == "!value-a" {
-		if len(parts) >= 2 {
-			if err := m.setParameter("A", parts[1]); err != nil {
-				return twitchactions.Mention(user, err.Error())
-			}
-
-			err := m.Save()
-			if err != nil {
-				return nil
-			}
-			return twitchactions.Mention(user, "A set to "+utils.Float32ToString(m.A))
-		}
-
-		return twitchactions.Mention(user, "A is "+utils.Float32ToString(m.A))
-	}
-
-	if parts[0] == "!value-b" {
-		if len(parts) >= 2 {
-			if err := m.setParameter("B", parts[1]); err != nil {
-				return twitchactions.Mention(user, err.Error())
-			}
-
-			err := m.Save()
-			if err != nil {
-				return nil
-			}
-			return twitchactions.Mention(user, "B set to "+utils.Float32ToString(m.B))
-		}
-
-		return twitchactions.Mention(user, "B is "+utils.Float32ToString(m.B))
-	}
-
-	return nil
+	return m.commands.OnMessage(event)
 }
