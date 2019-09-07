9834fd24b607e720fe0fd030832942edc1e47105
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-09-07 14:28:02 +0200

    add pb2exec command
    This allows anyone with the "moderation" permission to execute
    moderation commands using the bot account.
    
    Fixes #65

--- pkg/modules/debug.go
+++ pkg/modules/debug.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/pajbot/pajbot2/pkg"
@@ -60,6 +61,53 @@ func (c pb2Whisper) Trigger(parts []string, event pkg.MessageEvent) pkg.Actions 
 	return twitchactions.DoWhisper(targetUser, strings.Join(parts[2:], " "))
 }
 
+var (
+	whitelistedCommands = map[string]interface{}{
+		"ban":            nil,
+		"unban":          nil,
+		"timeout":        nil,
+		"untimeout":      nil,
+		"delete":         nil,
+		"subscribers":    nil,
+		"subscribersoff": nil,
+		"r9kbeta":        nil,
+		"r9kbetaoff":     nil,
+		"emoteonly":      nil,
+		"emoteonlyoff":   nil,
+		"slow":           nil,
+		"slowoff":        nil,
+		"followers":      nil,
+		"followersoff":   nil,
+		"clear":          nil,
+	}
+)
+
+type pb2Exec struct {
+}
+
+func (c *pb2Exec) Trigger(parts []string, event pkg.MessageEvent) pkg.Actions {
+	if !event.User.HasPermission(event.Channel, pkg.PermissionModeration) {
+		return nil
+	}
+
+	if len(parts) < 2 {
+		return nil
+	}
+
+	command := parts[1]
+	if !strings.HasPrefix(command, ".") && !strings.HasPrefix(command, "/") {
+		command = "." + command
+	}
+
+	if _, ok := whitelistedCommands[command[1:]]; !ok {
+		return twitchactions.DoWhisperf(event.User, "You are not allowed to run the command '%s'", command[1:])
+	}
+
+	fmt.Printf("pb2exec Executing command for %s: %s\n", event.User.GetName(), strings.Join(parts[1:], " "))
+
+	return twitchactions.Sayf("%s %s", command, strings.Join(parts[2:], " "))
+}
+
 type debugModule struct {
 	mbase.Base
 
@@ -82,6 +130,7 @@ func newDebugModule(b mbase.Base) pkg.Module {
 func (m *debugModule) Initialize() {
 	m.commands.Register([]string{"!pb2say"}, &pb2Say{})
 	m.commands.Register([]string{"!pb2whisper"}, &pb2Whisper{})
+	m.commands.Register([]string{"!pb2exec"}, &pb2Exec{})
 }
 
 func (m *debugModule) OnWhisper(event pkg.MessageEvent) pkg.Actions {
