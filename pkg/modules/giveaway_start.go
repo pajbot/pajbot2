a1938f2b3105a7ced046dcc9625c38d85cea70f4
alazymeme <jkeasley@icloud.com>
2019-11-03 22:47:55 +1000

    better error for unconfigured giveaway module (#80)
    Moved modules that don't work into an obiously named folder
    Slightly changed responses to the giveaway module (and moved it to the broken folder)
    Renamed modules to match their module id (to make enabling/disabling easier)

--- pkg/modules/giveaway_start.go
+++ pkg/modules/giveaway_start.go
@@ -18,6 +18,10 @@ func (c *giveawayCmdStart) Trigger(parts []string, event pkg.MessageEvent) pkg.A
 	if c.m.emoteID == "" {
 		return twitchactions.Mention(event.User, "No emote ID set. Use '!25config emoteid 98374583' to configure this module")
 	}
+	
+	if c.m.emoteName == "" {
+		return twitchactions.Mention(event.User, "No emote name set. Use '!25config emotename NaM' to configure this module")
+	}
 
 	if c.m.stopped() {
 		c.m.start()
