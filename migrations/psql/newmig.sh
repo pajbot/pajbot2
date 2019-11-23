7f5a732bbb92d56ea40705261f69d32970834724
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-22 21:54:38 -0800

    output usage instructions of newmig.sh to stderr

--- migrations/psql/newmig.sh
+++ migrations/psql/newmig.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-script_path=$(dirname "$0")
-description=$1
+script_path="$(dirname "$0")"
+description="$1"
 
 if [ -z "$description" ]; then
-    echo "usage: $0 description-of-migration"
+    >&2 echo "usage: $0 description-of-migration"
     exit 1
 fi
 
