6d9885ac0a68488bd018d7710b0faa361c006455
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-24 12:11:41 +0100

    Remove unused function isValidURL

--- pkg/web/errors.go
+++ pkg/web/errors.go
@@ -10,16 +10,8 @@ const (
 var (
 	singleUserName = regexp.MustCompile(`\w+`)
 	userNameList   = regexp.MustCompile(`[\w\,]+`)
-	rawURL         = regexp.MustCompile(`[\w\,\/]+`)
 )
 
 func isValidUserName(input string) bool {
 	return singleUserName.FindString(input) == input
-}
-
-func isValidURL(url string) bool {
-	if rawURL.FindString(url) != url {
-		return false
-	}
-	return true
 }
