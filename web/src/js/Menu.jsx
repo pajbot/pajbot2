bf6060a50b671ff9ed53e6ee3b1a0f6790badde1
Leppunen <niko@ivr.fi>
2020-08-15 16:38:28 +0300

    Update navbar classes one more time

--- web/src/js/Menu.jsx
+++ web/src/js/Menu.jsx
@@ -22,7 +22,7 @@ export default class Menu extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="#">pajbot2</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
