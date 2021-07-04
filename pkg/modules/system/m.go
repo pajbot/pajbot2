e0a6b68e77648948bc5ed88a4a86ab6fba5d38ca
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-07-04 15:25:40 +0200

    Add missing files

---
+++ pkg/modules/system/m.go
@@ -0,0 +1,50 @@
+package system
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/nicklaw5/helix"
+	"github.com/pajbot/pajbot2/pkg"
+	"github.com/pajbot/pajbot2/pkg/modules"
+	mbase "github.com/pajbot/pajbot2/pkg/modules/base"
+	"github.com/pajbot/pajbot2/pkg/twitchactions"
+)
+
+const id = "system"
+const name = "System"
+
+func init() {
+	modules.Register(id, func() pkg.ModuleSpec {
+		return modules.NewSpec(id, name, true, newModule)
+	})
+}
+
+type module struct {
+	mbase.Base
+}
+
+func newModule(b *mbase.Base) pkg.Module {
+	m := &module{
+		Base: *b,
+	}
+
+	return m
+}
+
+func (m *module) OnEventSubNotification(event pkg.EventSubNotificationEvent) pkg.Actions {
+	switch event.Notification.Subscription.Type {
+	case helix.EventSubTypeChannelFollow:
+		var followEvent helix.EventSubChannelFollowEvent
+		err := json.NewDecoder(bytes.NewReader(event.Notification.Event)).Decode(&followEvent)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		return twitchactions.Sayf("%s just followed the stream xD", followEvent.UserLogin)
+	default:
+		fmt.Println("Got unhandled eventsub notification:", event)
+	}
+	return nil
+}
