4dca1c78c216ae917933b43542d1997a2fc87b17
pajlada <rasmus.karlsson@pajlada.com>
2021-09-04 13:05:11 +0200

    Remove terser webpack plugin from optimizations since it's also default (#560)

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 
-const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,9 +8,6 @@ const BUILD_DIR = path.resolve(__dirname, './static/build');
 
 module.exports = {
 	entry: './src/index.jsx',
- 	optimization: {
-		minimizer: [new TerserJSPlugin({})]
-	},
 	output: {
 		path: BUILD_DIR,
 		filename: '[name].[contenthash].js',
