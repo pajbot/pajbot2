109e6fdaf47857e71ef53275049f99aa992e351b
pajlada <rasmus.karlsson@pajlada.com>
2025-03-17 21:01:14 +0100

    fix website (#1326)

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Dashboard from './js/Dashboard';
 import Banphrases from './js/Banphrases';
 import Admin from './js/Admin';
@@ -15,15 +15,18 @@ const banphrases = document.getElementById('banphrases');
 const admin = document.getElementById('admin');
 const commands = document.getElementById('commands');
 
-const App = (
-  <ThemeProvider>
-    <ThemeLoader />
-    <Menu />
-    {dashboard && <Dashboard element={dashboard} />}
-    {banphrases && <Banphrases />}
-    {admin && <Admin element={admin} />}
-    {commands && <Commands element={commands} />}
-  </ThemeProvider>
-);
+function App() {
+  return (
+    <ThemeProvider>
+      <ThemeLoader />
+      <Menu />
+      {dashboard && <Dashboard element={dashboard} />}
+      {banphrases && <Banphrases />}
+      {admin && <Admin element={admin} />}
+      {commands && <Commands element={commands} />}
+    </ThemeProvider>
+  );
+}
 
-ReactDOM.render(App, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
